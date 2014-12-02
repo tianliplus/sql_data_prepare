48cb259aea857ce10a0670d01ac0c6d433baadb5
Tian Li <tianli.tl@alibaba-inc.com>
2014-12-02 22:36:38 +0800

    first commit

---
+++ prepare_data.py
@@ -0,0 +1,40 @@
+__author__ = 'tianli'
+
+'''
+Table : h25v03
+
++----+------+------+--------+--------+--------+--------+--------+--------+--------+--------+---------+------+--------+--------+-----------+------+---------+------+--------+
+| id | date | line | sample | nbar_1 | nbar_2 | nbar_3 | nbar_4 | nbar_5 | nbar_6 | nbar_7 | nbar_qc | lai  | lai_qc | laimod | laimod_qc | ndvi | ndvi_qc | lst  | lst_qc |
++----+------+------+--------+--------+--------+--------+--------+--------+--------+--------+---------+------+--------+--------+-----------+------+---------+------+--------+
+|  1 |    1 |    2 |      3 |   NULL |   NULL |   NULL |   NULL |   NULL |   NULL |   NULL |    NULL | NULL |   NULL |   NULL |      NULL | NULL |    NULL | NULL |   NULL |
++----+------+------+--------+--------+--------+--------+--------+--------+--------+--------+---------+------+--------+--------+-----------+------+---------+------+--------+
+
+
+"date","line","sample", ... ...
+'''
+
+import os.path
+import sys
+
+# 数据文件路径
+__datadir = sys.argv[1]
+# 存放结果路径
+__tmpfile = sys.argv[2]
+
+fields_dsname = {
+    "nbar_1": "test_1",
+    "nbar_2": "test_2"
+}
+
+lai_files = filter(lambda x: "MCD15A2" in x, os.listdir(os.path.join(__datadir, "lai")))
+
+tmp_data_file = open(__tmpfile, 'w')
+
+def put_item(item):
+    tmp_data_file.write("\"{0}\"".format(item))
+
+
+
+
+
+tmp_data_file.close()
---
+++ table_field.py
@@ -0,0 +1,8 @@
+__author__ = 'tianli'
+
+class TableField:
+    '''
+    A field class, contains:
+        - field name: date, line, sample, nbar_1, nbar_2, ......
+        -
+    '''
